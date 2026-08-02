61be59b50b4985b4d1716b8d6d5e55b2e285230d
PratikMhaske-PM <pratikmhaske851@gmail.com>
2026-08-02 14:54:51 +0530

    Ready for deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api', 
-    'https://finflow-czwb.onrender.com/'// Replace with your backend API URL
+    baseURL: 'https://finflow-czwb.onrender.com/',// Replace with your backend API URL
     headers: {
         'Content-Type': 'application/json'
     },
