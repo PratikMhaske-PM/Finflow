7e8a7a66eb97c37e4b06de9ee534319a6c41130c
PratikMhaske-PM <pratikmhaske851@gmail.com>
2026-08-02 15:17:57 +0530

    Update API base URL to include '/api' path

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://finflow-czwb.onrender.com/',// Replace with your backend API URL
+    baseURL: 'https://finflow-czwb.onrender.com/api',// Replace with your backend API URL
     headers: {
         'Content-Type': 'application/json'
     },
